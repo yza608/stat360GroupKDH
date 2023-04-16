9a4c19bff203667d2ce0cd2f75b546ec61128d23
Daniel Z <danielzhiyd@hotmail.com>
2023-04-16 13:47:54 -0700

    Update test.R

--- test.R
+++ test.R
@@ -1,4 +1,7 @@
 library(mars)
+
+
+#test using data2
 test1 <- mars(y~., data1[sample(nrow(data1), 100),], mars.control(Mmax=6))
 print(test1)
 summary(test1)
@@ -6,6 +9,7 @@ plot(test1)
 anova(test1)
 predict(test1,data1[sample(nrow(data1), 100),2:10])
 
+#test using data2
 test2 <- mars(y~., data2[sample(nrow(data2), 100),], mars.control(Mmax=2))
 print(test2)
 summary(test2)
@@ -13,7 +17,7 @@ plot(test2)
 anova(test2)
 predict(test2,data2[sample(nrow(data2), 100),1:6])
 
-
+#test using data3
 test3 <- mars(y~., data3[sample(nrow(data3), 100),], mars.control(Mmax=4))
 print(test3)
 summary(test3)
